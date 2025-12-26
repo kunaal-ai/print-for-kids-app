b1495013f056c6de6ab230cb370bec0ce8057875
Kunaal Thanik <kunaal@gmx.com>
2025-12-26 17:13:02 -0600

    fix: Add material-icons-extended dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.navigation:navigation-compose:2.7.5")
     
     testImplementation("junit:junit:4.13.2")
